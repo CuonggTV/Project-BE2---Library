8a878f024a4978ca14454438162a29801455da22
kayden <nguyendaithinh3004@gmail.com>
2023-09-24 14:29:41 +0700

    update admin menu

--- src/views/AdminMenu.java
+++ src/views/AdminMenu.java
@@ -3,29 +3,44 @@ package views;
 import controllers.CustomerController;
 
 public class AdminMenu extends AbstractMenu {
-    private final CustomerController customerController;
+    private final AdminView adminView;
     public final static String[] CHOICE_OPTIONS = {
             "Add books.",
             "Delete books",
             "Show all books.",
-            "Show all customers and their status(borrowed or not)."
+            "Show all customers and their status(borrowed or not).",
+            "Show customers with fines",
+            "Show customers with reservation",
+            "Show customers with amount of book"
     };
 
     public AdminMenu(String[] choiceOptions) {
         super(choiceOptions);
-        customerController = new CustomerController();
+        adminView = new AdminView();
     }
         @Override
         public void eventHandler(int whichChoice) {
             switch (whichChoice){
                 case 1:
-                    AdminView admin = new AdminView();
+                    adminView.addBook();
                     break;
                 case 2:
+                    adminView.deleteBook();
                     break;
                 case 3:
+                    adminView.showInfoOfAllBooks();
                     break;
                 case 4:
+                    adminView.showAllCustomerAndTheirStatus();
+                    break;
+                case 5:
+                    adminView.showAllCustomerWithFine();
+                    break;
+                case 6:
+                    adminView.showAllCustomerWithReservation();
+                    break;
+                case 7:
+                    adminView.addBookAmount();
                     break;
             }
         }
--- src/views/Mainn.java
+++ src/views/Mainn.java
@@ -8,9 +8,11 @@ public class Mainn {
     public static void main(String[] args) throws SQLException {
 
         Mysql.getConnected();
-        AdminView bookView = new AdminView();
-//        bookView.showBorrowedBook();
-        CustomerView customerView = new CustomerView();
-        bookView.showAllCustomerAndTheirStatus();
+//        AdminView bookView = new AdminView();
+////        bookView.showBorrowedBook();
+//        CustomerView customerView = new CustomerView();
+//        bookView.showAllCustomerAndTheirStatus();
+//            AdminMenu menu = new AdminMenu();
+
     }
 }
